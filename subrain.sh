be6164e47978d922704c42d476d2fac46ecaf0bc
dials <tastywaves56k@gmail.com>
2022-01-14 16:19:36 -0800

    removed third level enum

--- subrain.sh
+++ subrain.sh
@@ -17,19 +17,7 @@ elif [ $ == "-h" ]
 		echo "create file with at least one domain listed, seperated by newline"
 		echo "./subrain <file_you_just_created>"
 else
-	echo -e '
-  ██████  █    ██  ▄▄▄▄    ██▀███   ▄▄▄       ██▓ ███▄    █ 
-▒██    ▒  ██  ▓██▒▓█████▄ ▓██ ▒ ██▒▒████▄    ▓██▒ ██ ▀█   █ 
-░ ▓██▄   ▓██  ▒██░▒██▒ ▄██▓██ ░▄█ ▒▒██  ▀█▄  ▒██▒▓██  ▀█ ██▒
-  ▒   ██▒▓▓█  ░██░▒██░█▀  ▒██▀▀█▄  ░██▄▄▄▄██ ░██░▓██▒  ▐▌██▒
-▒██████▒▒▒▒█████▓ ░▓█  ▀█▓░██▓ ▒██▒ ▓█   ▓██▒░██░▒██░   ▓██░
-▒ ▒▓▒ ▒ ░░▒▓▒ ▒ ▒ ░▒▓███▀▒░ ▒▓ ░▒▓░ ▒▒   ▓▒█░░▓  ░ ▒░   ▒ ▒ 
-░ ░▒  ░ ░░░▒░ ░ ░ ▒░▒   ░   ░▒ ░ ▒░  ▒   ▒▒ ░ ▒ ░░ ░░   ░ ▒░
-░  ░  ░   ░░░ ░ ░  ░    ░   ░░   ░   ░   ▒    ▒ ░   ░   ░ ░ 
-      ░     ░      ░         ░           ░  ░ ░           ░ 
-                        ░                                   '
-
-	
+		
 	echo
 	echo -e "running assetfinder on ${BOLDCYAN}${1}${NORM}"
 	# first round of domain enumeration
@@ -39,21 +27,9 @@ else
 	echo
 	echo -e "$(wc -l < $1_domains) domains are sitting in file ${BOLDCYAN}\"$1_domains\"${NORM}"
 	
-	# removes anything other than 'foo.domain.com' and reruns assetfinder, essentially a weird spidering replacement
-	# REMOVED - WILL REPLACE WITH SUBLIST3R	
-	# echo
-	# echo "Beginning recursive domain enumeration..."
-	# rm -f -- $1_thirdlevel.tmp
-	# grep -Po "(\w+\.\w+\.\w+)$" $1_domains | anewer $1_thirdlevel.tmp &>/dev/null
-	# echo
-	# echo "running assetfinder on 3rd level domains, this may take a while..."
-	# cat $1_thirdlevel.tmp | assetfinder | anewer $1_domains &>/dev/null
-	# echo 
-	# echo "$(wc -l < $1_domains) domains are now sitting in file \"$1_domains\""
-	# rm -r -- $1_thirdlevel.tmp
-
-	runs the output of the previous steps through httprobe
-	creates a new file named "hosts" with only live hosts
+	# runs the output of the previous steps through httprobe
+	# creates a new file named "hosts" with only live hosts
+	
 	cat $1_domains | httprobe -c 80 -prefer-https | anewer $1_hosts &>/dev/null
 	echo
 	echo -e "$(wc -l < $1_hosts) hosts are sitting in file ${BOLDCYAN}\"$1_hosts\"${NORM}"
